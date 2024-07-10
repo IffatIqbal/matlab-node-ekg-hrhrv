87b755c06d529ce5a952ea0b68102dec09132345
Iffat Iqbal <88254293+IffatIqbal@users.noreply.github.com>
2024-07-10 17:26:38 -0400

    Update readData.m

--- matlab/readData.m
+++ matlab/readData.m
@@ -1,8 +1,12 @@
 function data = readData(path)
     % Read ecg values and timestamps from file to array
-    file = fopen(path, 'r');
+    file = fopen(path, 'r'); % Open the file for reading
+    if file == -1
+        error('Error opening the file: %s', path);
+    end
+    
     formatSpec = '%f %f';
-    sizeA = [2,Inf];
+    sizeA = [2, Inf];
     d = fscanf(file, formatSpec, sizeA);
     fclose(file);
     data = d';
